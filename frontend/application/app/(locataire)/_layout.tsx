0432e882c2ef2aaec404eb939522158be40944eb
Baguimon <timothe.winkler@gmail.com>
2026-02-20 10:12:42 +0100

    Fix profile page and auto up

--- frontend/application/app/(locataire)/_layout.tsx
+++ frontend/application/app/(locataire)/_layout.tsx
@@ -23,6 +23,7 @@ export default function LocataireLayout() {
             <Tabs
                 screenOptions={{
                     headerShown: false,
+                    unmountOnBlur: true,
                     tabBarActiveTintColor: "#3153A1",
                     tabBarInactiveTintColor: "#7a7a7a",
                     tabBarStyle: {
